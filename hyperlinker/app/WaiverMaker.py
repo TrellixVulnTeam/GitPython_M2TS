842633f8d69a850e6e53fb71dd338abbef3da693
Jay Ackley <jayackley@gmail.com>
2021-09-21 11:15:15 -0400

    Started with template for WaiverMaker

---
+++ hyperlinker/app/WaiverMaker.py
@@ -0,0 +1,91 @@
+#some of these imports are extraneous and left over from the flask megatutorial 
+from flask import render_template, flash, redirect, url_for, request, Flask, jsonify, send_from_directory
+from app import app, db, DataWizardTools
+from app.models import User, Post
+from app.forms import PostForm
+from werkzeug.urls import url_parse
+from datetime import datetime
+import pandas as pd
+import os
+
+
+@app.route("/WaiverMaker", methods=['GET', 'POST'])
+def WaiverMaker():
+    #upload file from computer
+    if request.method == 'POST':
+        print(request.files['file'])
+        f = request.files['file']
+        
+        #turn the excel file into a dataframe, but skip the top 2 rows if they are blank
+        test = pd.read_excel(f)
+        test.fillna('',inplace=True)
+        if test.iloc[0][0] == '':
+            df = pd.read_excel(f,skiprows=2)
+            print("Skipped top two rows")
+        else:
+            df = pd.read_excel(f)
+            print("Dataframe starts from top")
+        
+        
+        #apply hyperlink methodology with splicing and concatenation
+      
+        def NoIDDelete(CaseID):
+            if CaseID == '':
+                return 'No Case ID'
+            else:
+                return str(CaseID)
+        df['Matter/Case ID#'] = df.apply(lambda x: NoIDDelete(x['Matter/Case ID#']), axis=1)
+        
+        last7 = df['Matter/Case ID#'].apply(lambda x: x[3:])
+        CaseNum = df['Matter/Case ID#']
+        df['Temp Hyperlinked Case #']='=HYPERLINK("https://lsnyc.legalserver.org/matter/dynamic-profile/view/'+last7+'",'+ '"' + CaseNum +'"' +')'
+        del df['Matter/Case ID#']
+        move=df['Temp Hyperlinked Case #']
+        df.insert(0,'Hyperlinked Case #', move)           
+        del df['Temp Hyperlinked Case #']
+        
+        """
+        #Remove Rows without Case ID values
+        df.fillna('',inplace = True)
+        df['Matter/Case ID#'] = df.apply(lambda x : DataWizardTools.RemoveNoCaseID(x['Matter/Case ID#']),axis=1)        
+        df = df[df['Matter/Case ID#'] != 'No Case ID']
+        
+        #Create Hyperlinks
+        df['Hyperlinked CaseID#'] = df.apply(lambda x : DataWizardTools.Hyperlinker(x['Matter/Case ID#']),axis=1)    
+        """
+        
+        #bounce worksheets back to excel
+        output_filename = f.filename     
+        writer = pd.ExcelWriter("app\\sheets\\"+output_filename, engine = 'xlsxwriter')
+        df.to_excel(writer, sheet_name='Sheet1',index=False)
+        workbook = writer.book
+        worksheet = writer.sheets['Sheet1']
+        
+        #create format that will make case #s look like links
+        link_format = workbook.add_format({'font_color':'blue', 'bold':True, 'underline':True})
+        
+        #assign new format to column A
+        worksheet.set_column('A:A',20,link_format)
+        
+        writer.save()
+        
+        #send file back to user
+        return send_from_directory('sheets',output_filename, as_attachment = True, attachment_filename = "Hyperlinked " + f.filename)
+        
+#what the user-facing site looks like
+    return '''
+    <!doctype html>
+    <title>Hyperlinks</title>
+    <link rel="stylesheet" href="/static/css/main.css">  
+    <link rel="stylesheet" href="/static/css/main.css"> 
+    <h1>Add Hyperlinks to Report:</h1>
+    <form action="" method=post enctype=multipart/form-data>
+    <p><input type=file name=file><input type=submit value=Hyperlink!>
+    </form>
+    <h3>Instructions:</h3>
+    <ul type="disc">
+    <li>This tool is meant to be used in conjunction with the LegalServer report called <a href="https://lsnyc.legalserver.org/report/dynamic?load=1507" target="_blank">TRC Raw Case Data Report</a>.</li>
+    </br>
+    <a href="/">Home</a>
+    '''
+    
