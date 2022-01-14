b42ed7ad7efafd8816608ab78a25defdb14103bb
Jay Ackley <jayackley@gmail.com>
2022-01-14 10:49:34 -0500

    create zipped folder functionality

--- hyperlinker/app/BoroughSplitter.py
+++ hyperlinker/app/BoroughSplitter.py
@@ -1,12 +1,13 @@
 #some of these imports are extraneous and left over from the flask megatutorial 
 from flask import render_template, flash, redirect, url_for, request, Flask, jsonify, send_from_directory
-from app import app, db
+from app import app, db, DataWizardTools
 from app.models import User, Post
 from app.forms import PostForm
 from werkzeug.urls import url_parse
 from datetime import datetime
 import pandas as pd
 import os
+from zipfile import ZipFile
 
 
 @app.route("/BoroughSplitter", methods=['GET', 'POST'])
@@ -20,86 +21,70 @@ def split_by_borough():
         test = pd.read_excel(f)
         test.fillna('',inplace=True)
         if test.iloc[0][0] == '':
-            data_xls = pd.read_excel(f,skiprows=2)
+            df = pd.read_excel(f,skiprows=2)
         else:
-            data_xls = pd.read_excel(f)
+            df = pd.read_excel(f)
         
 
-        #apply hyperlink methodology with splicing and concatenation
+        #delete blank rows
       
-        def NoIDDelete(CaseID):
-            if CaseID == '':
-                return 'No Case ID'
-            else:
-                return str(CaseID)
-        data_xls['Matter/Case ID#'] = data_xls.apply(lambda x: NoIDDelete(x['Matter/Case ID#']), axis=1)
+        df.fillna('',inplace = True)
+        df['Matter/Case ID#'] = df.apply(lambda x : DataWizardTools.RemoveNoCaseID(x['Matter/Case ID#']),axis=1)        
+        df = df[df['Matter/Case ID#'] != 'No Case ID']
         
-        last7 = data_xls['Matter/Case ID#'].apply(lambda x: x[3:])
-        CaseNum = data_xls['Matter/Case ID#']
-        data_xls['Temp Hyperlinked Case #']='=HYPERLINK("https://lsnyc.legalserver.org/matter/dynamic-profile/view/'+last7+'",'+ '"' + CaseNum +'"' +')'
-        del data_xls['Matter/Case ID#']
-        move=data_xls['Temp Hyperlinked Case #']
-        data_xls.insert(0,'Hyperlinked Case #', move)           
-        del data_xls['Temp Hyperlinked Case #']
+        #hyperlink
+        df['Matter/Case ID#'] = df.apply(lambda x : DataWizardTools.Hyperlinker(x['Matter/Case ID#']),axis=1)    
         
-        
-        #split into separate dataframes
-        
-        BkLSdata_xls = data_xls[data_xls['Assigned Branch/CC'] == 'Brooklyn Legal Services']
-        BxLSdata_xls = data_xls[data_xls['Assigned Branch/CC'] == 'Bronx Legal Services']
-        QLSdata_xls = data_xls[data_xls['Assigned Branch/CC'] == 'Queens Legal Services']
-        MLSdata_xls = data_xls[data_xls['Assigned Branch/CC'] == 'Manhattan Legal Services']
-        SILSdata_xls = data_xls[data_xls['Assigned Branch/CC'] == 'Staten Island Legal Services']
-        LSUdata_xls = data_xls[data_xls['Assigned Branch/CC'] == 'Legal Support Unit']
-        
-        #bounce worksheets back to excel
-        output_filename = f.filename     
-        writer = pd.ExcelWriter("app\\sheets\\"+output_filename, engine = 'xlsxwriter')
-        
-        BkLSdata_xls.to_excel(writer, sheet_name='Brooklyn',index=False)
-        QLSdata_xls.to_excel(writer, sheet_name='Queens', index=False)
-        MLSdata_xls.to_excel(writer, sheet_name='Manhattan', index=False)
-        BxLSdata_xls.to_excel(writer, sheet_name='Bronx', index=False)
-        SILSdata_xls.to_excel(writer, sheet_name='Staten Island', index=False)
-        LSUdata_xls.to_excel(writer, sheet_name='LSU', index=False)
-        
-        
-        #rename worksheets with their original names 
-        workbook = writer.book
-        
-        worksheetBkLS = writer.sheets['Brooklyn']
-        worksheetQLS = writer.sheets['Queens']
-        worksheetMLS = writer.sheets['Manhattan']
-        worksheetBxLS = writer.sheets['Bronx']
-        worksheetSILS = writer.sheets['Staten Island']
-        worksheetLSU = writer.sheets['LSU']
-        
-        link_format = workbook.add_format({'font_color':'blue', 'bold':True, 'underline':True})
-        
-        worksheetBkLS.freeze_panes(1, 0)
-        worksheetQLS.freeze_panes(1, 0)
-        worksheetMLS.freeze_panes(1, 0)
-        worksheetBxLS.freeze_panes(1, 0)
-        worksheetSILS.freeze_panes(1, 0)
-        worksheetLSU.freeze_panes(1, 0)
-        worksheetBkLS.set_column('A:A',20,link_format)
-        worksheetQLS.set_column('A:A',20,link_format)
-        worksheetMLS.set_column('A:A',20,link_format)
-        worksheetBxLS.set_column('A:A',20,link_format)
-        worksheetSILS.set_column('A:A',20,link_format)
-        worksheetLSU.set_column('A:A',20,link_format)
-        worksheetBkLS.set_column('B:ZZ',25)
-        worksheetQLS.set_column('B:ZZ',25)
-        worksheetMLS.set_column('B:ZZ',25)
-        worksheetBxLS.set_column('B:ZZ',25)
-        worksheetSILS.set_column('B:ZZ',25)
-        worksheetLSU.set_column('B:ZZ',25)
-        
-        writer.save()
-        
-        #send file back to user
-        return send_from_directory('sheets',output_filename, as_attachment = True, attachment_filename = "Split " + f.filename)
+        #Preparing Excel Document
 
+        
+        
+        #define function, with single variable of dictionary created immediately above
+        def save_xls(ExcelSplit, TabSplit):
+            #Create dictionary of dataframes, each named after each unique value of 'assigned branch/cc'
+            excel_dict_df = dict(tuple(df.groupby(ExcelSplit)))
+            #use 'ZipFile' create empty zip folder, and assign 'newzip' as function-calling name
+            with ZipFile("app\\sheets\\zipped\\SplitBoroughs.zip","w") as newzip:
+                
+                #starts cycling through each dataframe (each borough's data)
+                for i in excel_dict_df:
+                    
+                    #because this is in for loop it creates a new excel file for each 'i' (i.e. each borough)
+                    writer = pd.ExcelWriter(path = "app\\sheets\\zipped\\" + i + ".xlsx", engine = 'xlsxwriter')
+                    
+                    #create a dictionary of dataframes for each unique advocate, within the borough that our 'i' for loop is cycling through
+                    tab_dict_df = dict(tuple(excel_dict_df[i].groupby(TabSplit)))
+                    
+                    #start cycling through each of these new advocate-based dataframes
+                    for j in tab_dict_df:
+                        
+                        #write a tab in the borough's excel sheet, composed just of the advocate's cases, with the advocate's name
+                        tab_dict_df[j].to_excel(writer, j, index = False)
+                        
+                        #creates ability to format tabs
+                        worksheet = writer.sheets[j]
+                        
+                        #creates and adds formatting to tab
+                        workbook = writer.book
+                        link_format = workbook.add_format({'font_color':'blue','bold':True,'underline':True})
+                        worksheet.set_column('A:A',15,link_format)
+                        worksheet.set_column('B:ZZ',20)
+                        
+                    #save's excel file
+                    writer.save()
+                    #adds excel file to zipped folder
+                    newzip.write("app\\sheets\\zipped\\" + i + ".xlsx",arcname = i + ".xlsx")
+          
+        save_xls(ExcelSplit = 'Assigned Branch/CC', TabSplit = 'Caseworker Name')
+        
+        
+            
+            #newzip.write("app\\sheets\\zipped\\Brooklyn Legal Services ZipSplitterTester.xlsx", arcname = "Brooklyn Legal ServicesZipSplitterTester.xlsx")
+            #newzip.write("app\\sheets\\zipped\\Queens Legal Services ZipSplitterTester.xlsx", arcname = "Queens Legal ServicesZipSplitterTester.xlsx")
+            
+        
+        return send_from_directory('sheets\\zipped','SplitBoroughs.zip', as_attachment = True)
+        
         
 #what the user-facing site looks like
     return '''
